c209d0b860781c90fc6e0a4f8a68ce865724551e
Nikita Mamro <mamronikita@gmail.com>
2018-10-06 12:31:49 +0300

    Added quicksort and initialized function for the task.

--- Lab3/Task3/HomeWork/Source.cpp
+++ Lab3/Task3/HomeWork/Source.cpp
@@ -24,6 +24,35 @@ void printArray(int *theArray, int length)
 	printf("\n\n");
 }
 
+void quickSort(int* arr, int firstIndex, int lastIndex)
+{
+	if (firstIndex >= lastIndex)
+	{
+		return;
+	}
+	int index = firstIndex;
+	int element = arr[firstIndex];
+	for (int i = firstIndex; i <= lastIndex; ++i)
+	{
+		if (arr[i] < element)
+		{
+			swap(arr[i], arr[index]);
+			++index;
+		}
+	}
+	if (index == firstIndex)
+	{
+		++index;
+	}
+	quickSort(arr, firstIndex, index - 1);
+	quickSort(arr, index, lastIndex);
+}
+
+int mostFrequentElement(int *theArray, int length)
+{
+
+}
+
 int main()
 {
 	srand(time(nullptr));
