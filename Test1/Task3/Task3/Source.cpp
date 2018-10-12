83cb8db5f2dc2800e8b6ed9f4ff1af26581073e2
Nikita Mamro <mamronikita@gmail.com>
2018-10-12 19:02:00 +0300

    Some crap

---
+++ Test1/Task3/Task3/Source.cpp
@@ -0,0 +1,52 @@
+#include <conio.h>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int* readFromFile()
+{
+	int *result = new int[101];
+	ifstream inf("ololo.txt");
+	int input = 0;
+	int i = 0, j = 0;
+	while (!inf.eof())
+	{
+		inf >> input;
+		if (input != ' ')
+		{
+			result[i] = input;
+			cout << input;
+			++j;
+		}
+		++i;
+	}
+	result[101] = j;
+	return result;
+}
+
+void writeToFile(int *data, int length)
+{
+	ofstream of("ololo.txt");
+	int *buffer = new int[length + 1];
+	int j = length - 1;
+	for (int i = 0; i < length; ++i)
+	{
+		//of << data[i] << ' ';
+		buffer[j] = data[i];
+		j--;
+	}
+	for (int i = 0; i < length; ++i)
+	{
+		of << buffer[i] << ' ';
+	}
+}
+
+int main()
+{
+	int *data = readFromFile();
+	writeToFile(data, data[101]);
+	delete[] data;
+	system("PAUSE");
+	return 0;
+}
