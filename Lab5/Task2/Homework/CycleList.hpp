b9165785a6d044585a5816512758c4e4fbf2c543
Nikita Mamro <mamronikita@gmail.com>
2018-10-25 20:37:45 +0300

    Lab5 Task2 without tests

---
+++ Lab5/Task2/Homework/CycleList.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "stdafx.hpp"
+
+struct ListElement
+{
+	int data = 0;
+	ListElement *next = nullptr;
+};
+
+struct CycleList
+{
+	ListElement *head = nullptr;
+	ListElement *tail = nullptr;
+	int length = 0;
+};
+
+void printCycleList(CycleList&);
+void addElement(int, CycleList&);
+void kill(int, CycleList&);
+ListElement *newElement(int, ListElement*);
