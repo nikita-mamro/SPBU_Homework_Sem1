46f4f73b1ea16bb093167208770fbee08ebb70c7
Nikita Mamro <mamronikita@gmail.com>
2018-11-07 21:05:36 +0300

    Memory leak fixed

--- Lab3/Task3/HomeWork/Source.cpp
+++ Lab3/Task3/HomeWork/Source.cpp
@@ -95,6 +95,7 @@ void test()
 	int* testArray = new int[16]{ 100, 8123, 31, 8213, 52, 3, 8123, 3, 24803, 24803, 3, 1, 90, 90, 8123, 8123 };
 	printArray(testArray, 16);
 	int result = mostFrequentElement(testArray, 16);
+	delete[] testArray;
 	printf("Подсчитанный элемент: %d\n", result);
 	result == 8123 ? printf("Тест пройден!\n\n") : printf("Тест не пройден, ответ 8123\n\n");
 }
