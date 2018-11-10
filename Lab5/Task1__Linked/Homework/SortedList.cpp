1fa49162ab940b1661bde3435997c5b6b9d7032d
Nikita Mamro <mamronikita@gmail.com>
2018-11-10 18:46:20 +0300

    5.1

---
+++ Lab5/Task1__Linked/Homework/SortedList.cpp
@@ -0,0 +1,126 @@
+#include "SortedList.hpp"
+
+bool isContained(int element, SortedList &list)
+{
+	ListElement *currentElement = list.head;
+	
+	while (currentElement != nullptr)
+	{
+		if (currentElement->data == element)
+		{
+			return true;
+		}
+		currentElement = currentElement->next;
+	}
+
+	return false;
+}
+
+bool isEmpty(SortedList &list)
+{
+	return list.head == nullptr;
+}
+
+ListElement *newListElement(int data, ListElement *nextElement)
+{
+	ListElement *result = new ListElement;
+	result->data = data;
+	result->next = nextElement;
+	return result;
+}
+
+ListElement* lastLessThanCurrent(int element, SortedList &list)
+{
+	if (list.head == nullptr || list.head->data >= element)
+	{
+		return nullptr;
+	}
+
+	ListElement *previousElement = list.head;
+
+	while (previousElement->next != nullptr && previousElement->next->data < element)
+	{
+		previousElement = previousElement->next;
+	}
+
+	return previousElement;
+}
+
+void addElement(int element, SortedList &list)
+{
+	ListElement* previousElement = lastLessThanCurrent(element, list);
+	if (previousElement != nullptr)
+	{
+		previousElement->next = newListElement(element, previousElement->next);
+	}
+	else
+	{
+		list.head = newListElement(element, list.head);
+	}
+	printf("Добавлен элемент %d!\n", element);
+}
+
+void removeElement(int element, SortedList &list)
+{
+	if (!isContained(element, list))
+	{
+		printf("Элемент не найден!\n");
+		return;
+	}
+
+	if (list.head == nullptr)
+	{
+		return;
+	}
+
+	ListElement *previousElement = lastLessThanCurrent(element, list);
+
+	if (previousElement == nullptr)
+	{
+		while (list.head->data == element && list.head != nullptr)
+		{
+			ListElement *tmp = list.head;
+			list.head = list.head->next;
+			delete tmp;
+		}
+	}
+	else
+	{
+		while (previousElement->next != nullptr && previousElement->next->data == element)
+		{
+			ListElement *tmp = previousElement->next;
+			previousElement->next = tmp->next;
+			delete tmp;
+		}
+	}
+	printf("Удалён элемент %d!\n", element);
+}
+
+void printSortedList(SortedList &list)
+{
+	ListElement *currentElement = list.head;
+
+	if (currentElement == nullptr)
+	{
+		printf("Нет элементов!\n");
+	}
+	else
+	{
+		while (currentElement != nullptr)
+		{
+			printf("%d ", currentElement->data);
+			currentElement = currentElement->next;
+		}
+		printf("\n");
+	}
+}
+
+void deleteSortedList(SortedList &list)
+{
+	while (!isEmpty(list))
+	{
+		ListElement *currentElement = list.head;
+		list.head = currentElement->next;
+		delete currentElement;
+	}
+}
---
+++ Lab5/Task1__Linked/Homework/Test.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "SortedList.hpp"
+#include <cstdlib>
+#include <locale.h>
+#include <time.h>
+
+bool test();
