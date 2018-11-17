54d94b7f46e06f912ef5e3797a1261bc50c4908c
Nikita Mamro <mamronikita@gmail.com>
2018-11-17 15:41:03 +0300

    Task 4.3 correct branch

---
+++ Lab4/Task3/Homework/File.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include "stdafx.hpp"
+#include "ContactStruct.hpp"
+
+void writeToFile(char[MAX_LENGTH], char[MAX_LENGTH]);
+int readFromFile(Contact*);
---
+++ Lab4/Task3/Homework/Phonebook.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "stdafx.hpp"
+#include "ContactStruct.hpp"
+
+void printAll();
+char* getPhoneNumber(char*);
+char* getName(char*);
+void saveData(Contact*, int);
