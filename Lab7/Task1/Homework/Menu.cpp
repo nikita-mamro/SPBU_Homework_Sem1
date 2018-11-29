1f6bcd59899a9317aee89e25b7c881751b4037d7
Nikita Mamro <mamronikita@gmail.com>
2018-11-30 00:22:48 +0300

    Moved including of module to cpp

--- Lab7/Task1/Homework/Menu.cpp
+++ Lab7/Task1/Homework/Menu.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <conio.h>
 #include "Menu.hpp"
+#include "Set.hpp"
 
 using namespace std;
 
--- Lab7/Task1/Homework/Menu.hpp
+++ Lab7/Task1/Homework/Menu.hpp
@@ -1,5 +1,4 @@
 #pragma once
-#include "Set.hpp"
 
 void printMenu();
 void proceedTask();
