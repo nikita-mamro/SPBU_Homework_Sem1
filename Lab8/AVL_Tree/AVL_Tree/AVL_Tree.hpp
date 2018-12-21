56df9d5eb5f3ca66c6c76ca098ddccd0a3319e56
Nikita Mamro <mamronikita@gmail.com>
2018-12-21 12:15:51 +0300

    Headers' comments' style (srry for Russian in comments)

--- Lab8/AVL_Tree/AVL_Tree/AVL_Tree.hpp
+++ Lab8/AVL_Tree/AVL_Tree/AVL_Tree.hpp
@@ -7,28 +7,38 @@ struct Node;
 Node *createNode();
 void deleteNode(Node *node);
 void deleteTree(Node *root);
+
 // Высота и баланс-фактор
 unsigned char height(Node *node);
 int balanceFactor(Node *node);
+
 // Устанавливаем корректное значение высоты узла после балансировки дочерних узлов
 void fixHeight(Node *node);
+
 // Повороты вокруг ноды, возвращают корень полученного дерева
 Node * rotateRight(Node * p);
 Node * rotateLeft(Node * p);
+
 // Балансировка узла p
 Node * balance(Node * p);
+
 // Добавление ключа
 Node * add(Node * root, int key, std::string str);
+
 // Поиск узла с минимальным ключом
 Node * findMin(Node *root);
+
 // Удаление
 Node * removeMin(Node *root);
 Node * remove(Node *root, int key);
+
 // Проверить ключ на существование
 bool exists(Node *root, int key);
+
 // Получить значение по ключу
 std::string getValue(Node *root, int key);
-// Функции для того, чтобы вынести тесты в отдельный модуль
+
+// Геттеры для тестов
 int getKey(Node *node);
 Node *getLeft(Node *node);
 Node *getRight(Node *node);
