4016a926e1afc237a77abffc3982ec7a04320ee3
Nikita Mamro <mamronikita@gmail.com>
2018-12-21 12:12:50 +0300

    Header's comments' style

--- Lab7/Task2/Homework/Tree.hpp
+++ Lab7/Task2/Homework/Tree.hpp
@@ -4,17 +4,22 @@
 
 struct Tree;
 struct Node;
+
 // Creating Node + Tree
 Node *createNode(std::ifstream &input);
 Tree *createTree(std::ifstream &input);
+
 // Deleting Node + Tree
 void deleteNode(Node *node);
 void deleteTree(Tree *tree);
+
 // Print expression
 void printExpression(Node *node);
 void printExpression(Tree *tree);
+
 // Proceed expression with 2 ariables and an operand
 int proceedOpernad(int a, int b, char operand);
+
 // Get value of an expression
 int getExpressionValue(Node *node);
 int getExpressionValue(Tree *tree);
