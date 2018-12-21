a1dc94ffe8a13bca50fc35c5b367926350ea2124
Nikita Mamro <mamronikita@gmail.com>
2018-12-21 12:23:47 +0300

    Comments' format

--- Lab10/Homework/Homework/Graph.hpp
+++ Lab10/Homework/Homework/Graph.hpp
@@ -5,14 +5,19 @@
 using namespace std;
 
 struct Node;
+
 // Create graphs of each country
 vector<Node*> getCapitalNodes(vector<int> &capitals, vector<InputElement*> &triplets);
+
 // Adding neighbours
 void addNewNeighbour(Node *node, int neighbour);
 void addExistingNeighbour(Node *node, Node *neighbour);
+
 // Check if cityA and cityB are int the same country or not
 bool differentCapitals(vector<Node*> &capitalNodes, int cityA, int cityB);
+
 // Deleting
 void deleteCountry(Node * capital);
+
 // Vector (countries) of vectors (cities)
 vector<vector<int>> solveTask(ifstream &input);
--- Lab10/Homework/Homework/ReadFromFile.hpp
+++ Lab10/Homework/Homework/ReadFromFile.hpp
@@ -8,5 +8,6 @@ void deleteInputElement(InputElement *element);
 int getRoadLength(InputElement *element);
 int getCityFrom(InputElement *element);
 int getCityTo(InputElement *element);
+
 // Read data from file into TRIPLETS array
 void readFromFile(std::vector<int> &capitals, std::vector<InputElement*> &triplets, std::ifstream & input);
