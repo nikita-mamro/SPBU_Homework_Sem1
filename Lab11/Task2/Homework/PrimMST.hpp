b6c4281fe6e8b27016bf71e73532247ad538c06f
Nikita Mamro <mamronikita@gmail.com>
2018-12-21 12:25:29 +0300

    Comments' format

--- Lab11/Task2/Homework/PrimMST.hpp
+++ Lab11/Task2/Homework/PrimMST.hpp
@@ -5,8 +5,10 @@ constexpr auto INF = 0x3F3F3F3F;
 
 // Reads graph implementation (adjacency matrix)
 void readFromFile(std::vector<std::vector<int>> & graph, std::ifstream &input);
+
 // Find vertex with minimum key which is NOT in tree
 int minKey(std::vector<int> &key, std::vector<bool> &set);
+
 // Parent p with inex i -> in spanning tree i is child of p
 // 0'th key is always root of spannin' tree
 void primMST(std::vector<int> &parents, std::vector<std::vector<int>> & graph);
--- Lab11/Task2/Homework/Tree.hpp
+++ Lab11/Task2/Homework/Tree.hpp
@@ -10,6 +10,7 @@ Tree * createTree(int rootKey);
 void add(Node * parent, Node * child, Tree * tree);
 
 Node *getByKey(int key, Node * root);
+
 // Create a tree of an array from Prim's algorithm
 void fillTree(std::vector<int> &parents, Tree * tree);
 
